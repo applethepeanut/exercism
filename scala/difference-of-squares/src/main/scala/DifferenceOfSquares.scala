8324ad7661b450c08755645f09b1c6fc309d8ebe
Andrew Smitherim <asmitherim@deloitte.co.uk>
2017-08-21 09:29:57 +0100

    Completed DifferenceOfSquares

--- scala/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
+++ scala/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
@@ -1,8 +1,11 @@
 object DifferenceOfSquares {
 
-  def sumOfSquares(n: Int): Int = ???
+  def sumOfSquares(n: Int): Int = (1 to n).map(i => i * i).sum
 
-  def squareOfSum(n: Int): Int = ???
+  def squareOfSum(n: Int): Int = {
+    val p = (1 to n).sum
+    p * p
+  }
 
-  def differenceOfSquares(n: Int): Int = ???
+  def differenceOfSquares(n: Int): Int = squareOfSum(n) - sumOfSquares(n)
 }
